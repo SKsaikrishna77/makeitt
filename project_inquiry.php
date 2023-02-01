d2f4cdd3ef333095e5d13e0ed9f04edbad2f3b87
Mohan Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-02-01 13:46:43 +0530

    Read More

--- project_inquiry.php
+++ project_inquiry.php
@@ -14,7 +14,7 @@
     </div>
       <div class="card-body box">
         <h4>Bomb detactor with Gps controller</h4>
-        <h6>To absorb technology and production of OPX-Revilator- an Optronics Explosive Detector <span class="text-primary" data-toggle="modal" data-target=".bd-example-modal-lg">more...</span></h6>
+        <h6>To absorb technology and production of OPX-Revilator- an Optronics Explosive Detector <span class="text-primary" data-toggle="modal" data-target=".bd-example-modal-lg" style="cursor: pointer;" > Read More...</span></h6>
         <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
           <div class="modal-dialog modal-lg">
             <div class="modal-content">
@@ -41,7 +41,7 @@
     </div>
     <div class="card-body box">
         <h4>Bomb detactor with Gps controller </h4>
-        <h6>To absorb technology and production of OPX-Revilator- an Optronics Explosive Detector <span class="text-primary" data-toggle="modal" data-target=".bd-example-modal-lg">more...</span></h6>
+        <h6>To absorb technology and production of OPX-Revilator- an Optronics Explosive Detector <span class="text-primary" data-toggle="modal" data-target=".bd-example-modal-lg" style="cursor: pointer; !important">Read More...</span></h6>
         <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
           <div class="modal-dialog modal-lg">
             <div class="modal-content">
@@ -67,7 +67,7 @@
     </div>
     <div class="card-body box">
         <h4>Bomb detactor with Gps controller </h4>
-        <h6>To absorb technology and production of OPX-Revilator- an Optronics Explosive Detector <span class="text-primary" data-toggle="modal" data-target=".bd-example-modal-lg">more...</span></h6>
+        <h6>To absorb technology and production of OPX-Revilator- an Optronics Explosive Detector <span class="text-primary" data-toggle="modal" data-target=".bd-example-modal-lg" style="cursor: pointer; !important">Read More...</span></h6>
         <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
           <div class="modal-dialog modal-lg">
             <div class="modal-content">
@@ -93,7 +93,7 @@
     </div>
     <div class="card-body box">
         <h4>Bomb detactor with Gps controller </h4>
-        <h6>To absorb technology and production of OPX-Revilator- an Optronics Explosive Detector <span class="text-primary" data-toggle="modal" data-target=".bd-example-modal-lg">more...</span></h6>
+        <h6>To absorb technology and production of OPX-Revilator- an Optronics Explosive Detector <span class="text-primary" data-toggle="modal" data-target=".bd-example-modal-lg" style="cursor: pointer; !important">Read More...</span></h6>
         <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
           <div class="modal-dialog modal-lg">
             <div class="modal-content">
@@ -120,7 +120,7 @@
     </div>
     <div class="card-body box">
         <h4>Bomb detactor with Gps controller </h4>
-        <h6>To absorb technology and production of OPX-Revilator- an Optronics Explosive Detector <span class="text-primary" data-toggle="modal" data-target=".bd-example-modal-lg">more...</span></h6>
+        <h6>To absorb technology and production of OPX-Revilator- an Optronics Explosive Detector <span class="text-primary" data-toggle="modal" data-target=".bd-example-modal-lg" style="cursor: pointer; !important">Read More...</span></h6>
         <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
           <div class="modal-dialog modal-lg">
             <div class="modal-content">
@@ -147,7 +147,7 @@
     </div>
     <div class="card-body box">
         <h4>Bomb detactor with Gps controller </h4>
-        <h6>To absorb technology and production of OPX-Revilator- an Optronics Explosive Detector <span class="text-primary" data-toggle="modal" data-target=".bd-example-modal-lg">more...</span></h6>
+        <h6>To absorb technology and production of OPX-Revilator- an Optronics Explosive Detector <span class="text-primary" data-toggle="modal" data-target=".bd-example-modal-lg" style="cursor: pointer; !important">Read More...</span></h6>
         <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
           <div class="modal-dialog modal-lg">
             <div class="modal-content">
@@ -174,7 +174,7 @@
     </div>
     <div class="card-body box">
         <h4>Bomb detactor with Gps controller </h4>
-        <h6>To absorb technology and production of OPX-Revilator- an Optronics Explosive Detector <span class="text-primary" data-toggle="modal" data-target=".bd-example-modal-lg">more...</span></h6>
+        <h6>To absorb technology and production of OPX-Revilator- an Optronics Explosive Detector <span class="text-primary" data-toggle="modal" data-target=".bd-example-modal-lg" style="cursor: pointer; !important">Read More...</span></h6>
         <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
           <div class="modal-dialog modal-lg">
             <div class="modal-content">
@@ -201,7 +201,7 @@
     </div>
     <div class="card-body box">
         <h4>Bomb detactor with Gps controller </h4>
-        <h6>To absorb technology and production of OPX-Revilator- an Optronics Explosive Detector <span class="text-primary" data-toggle="modal" data-target=".bd-example-modal-lg">more...</span></h6>
+        <h6>To absorb technology and production of OPX-Revilator- an Optronics Explosive Detector <span class="text-primary" data-toggle="modal" data-target=".bd-example-modal-lg" style="cursor: pointer; !important">Read More...</span></h6>
         <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
           <div class="modal-dialog modal-lg">
             <div class="modal-content">
@@ -228,8 +228,8 @@
     </div>
     <div class="card-body box">
         <h4>Bomb detactor with Gps controller </h4>
-        <h6>To absorb technology and production of OPX-Revilator- an Optronics Explosive Detector <span class="text-primary" data-toggle="modal" data-target=".bd-example-modal-lg">more...</span></h6>
-        <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <h6>To absorb technology and production of OPX-Revilator- an Optronics Explosive Detector  </br> <span class="text-primary" data-toggle="modal" data-target=".bd-example-modal-lg" style="cursor: pointer; " >Read More...</span></h6>
+        <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLabrgeModalLabel" aria-hidden="true">
           <div class="modal-dialog modal-lg">
             <div class="modal-content">
             <div class="modal-header">
